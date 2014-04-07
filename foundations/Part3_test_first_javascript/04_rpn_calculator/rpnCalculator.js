2b169f2cb4003ab30da33ab8305957ff7a1d685e
bado22 <abaden22@gmail.com>
2014-04-07 13:02:22 -0400

    Finished 04-rpnCalculator.js

---
+++ foundations/Part3_test_first_javascript/04_rpn_calculator/rpnCalculator.js
@@ -0,0 +1,49 @@
+function Calculator(){
+	this.nums = [];
+	this.display = 0;
+};
+
+Calculator.prototype.push = function(input){
+	this.nums.push(input);
+}
+
+Calculator.prototype.plus = function(){
+	this.checkNumStash()
+	if(this.nums.length === 0) throw "calculator is empty";
+	var firstNum = this.nums.pop(-1);
+	var secondNum = this.nums.pop(-1);
+	this.display = secondNum + firstNum;
+	this.nums[this.nums.length] = this.display;
+}
+
+Calculator.prototype.minus = function(){
+	this.checkNumStash()
+	var firstNum = this.nums.pop(-1);
+	var secondNum = this.nums.pop(-1) ;
+	this.display = secondNum - firstNum;
+	this.nums[this.nums.length] = this.display;
+}
+
+Calculator.prototype.divide = function(){
+	this.checkNumStash()
+	var firstNum = this.nums.pop(-1);
+	var secondNum = this.nums.pop(-1) ;
+	this.display = secondNum / firstNum;
+	this.nums[this.nums.length] = this.display;
+}
+
+Calculator.prototype.times = function(){
+	this.checkNumStash()
+	var firstNum = this.nums.pop(-1);
+	var secondNum = this.nums.pop(-1) ;
+	this.display = secondNum * firstNum;
+	this.nums[this.nums.length] = this.display;
+}
+
+Calculator.prototype.value = function(input){
+	return this.display;
+}
+
+Calculator.prototype.checkNumStash = function(){
+	if(this.nums.length === 0) throw "calculator is empty";
+}
