3c6300943bd11248c6107b25485eda1945713f04
bado22 <abaden22@gmail.com>
2014-04-12 12:56:20 -0400

    Re-did 08-mammal.js using the call method

--- foundations/Part3and4_test_first_javascript/08_mammals/mammal.js
+++ foundations/Part3and4_test_first_javascript/08_mammals/mammal.js
@@ -4,25 +4,24 @@ function Mammal(name){
 }
 
 function Cat(name, color){
-	Cat.prototype = new Mammal;
-	Cat.prototype.constructor = Mammal;
-	this.name = name;
-	this.color = color;
-	this.haveBaby = function(color){
-		var babyName = "Baby " + this.name;
-		var thisBaby = new Mammal(babyName);
-		thisBaby.color = color;
-		this.offspring.push(thisBaby);
-		return thisBaby;
-	}
+	this.prototype = Object.create(Mammal.prototype); // Sets the Cat prototype object equal to an object created based on the Mammal prototype.
+	this.prototype.constructor = Mammal; // Sets the constructor property equal to Mammal, since Cat is a subclass of Mammal.
+	Mammal.call(this, name); // Sets 'this' to this particular instance of cat & passes in the name variable, as Mammal requires.
+	this.color = color; // adds a color property to this Cat instance.
 }
 
 Mammal.prototype = {
 	sayHello: function(){return "My name is " + this.name + ", I'm a Mammal";},
 	haveBaby: function(){
 		var babyName = "Baby " + this.name;
-		var thisBaby = new Mammal(babyName);
-		this.offspring.push(thisBaby);
-		return thisBaby;
-	}	
+		var baby = new Mammal(babyName);
+		this.offspring.push(baby);
+		return baby;
+	}
+}
+
+Cat.prototype.haveBaby = function(color){
+	var catBaby = Mammal.prototype.haveBaby.call(this); // Calls the haveBaby function from the Mammal prototype & sets the result to catBaby
+	catBaby.color = color; // add the color property that was passed in as an argument to catBaby
+	return catBaby;
 }
